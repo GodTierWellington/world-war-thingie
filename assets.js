476d685b87b04fcb872bcaedde840b5c386260b4
Wellington <kesha.shpilevskiy@ucc.on.ca>
2018-07-24 18:38:33 -0400

    Addon 14

--- assets.js
+++ assets.js
@@ -3,7 +3,7 @@ function loadAssets () {
     pasha = loadImage ("https://raw.githubusercontent.com/GodTierWellington/world-war-waifu/master/assets/pasha.jpg")
     tower0 = loadImage("https://raw.githubusercontent.com/GodTierWellington/world-war-waifu/master/assets/level0.png")
     tower1 = loadImage("https://raw.githubusercontent.com/GodTierWellington/world-war-waifu/master/assets/level1.png")
-    textBox = loadImage ("https://raw.githubusercontent.com/GodTierWellington/world-war-waifu/master/assets/textbox.png")
+    textBox = loadImage ("https://raw.githubusercontent.com/GodTierWellington/world-war-waifu/master/assets/ui/textbox.png")
 
     //backgrounds
     moscow_b = loadImage ("https://raw.githubusercontent.com/GodTierWellington/world-war-waifu/master/assets/background/moscow.png")
